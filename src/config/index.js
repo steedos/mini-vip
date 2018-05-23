16a92282751dabbf5f96bdb3c1d32b7869894ac1
Jack Zhuang <zhuangjianguo@me.com>
2018-05-23 13:24:09 +0800

    加入演示工作区按钮

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,3 @@
-export const baseUrl = 'https://vip.steedos.com'
+export const baseUrl = 'https://vip.steedos.com:8443'
 export const appId = 'wxf7d7403acb34334b'
+export const demoSpaceId = 'hSGPc2fsMAp9NDcHy'
