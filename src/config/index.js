925ddd52b33c48aa48ca7ec16bdb5aee62304ac4
朱思嘉 <zhusijia@hotoa.com>
2018-05-24 09:39:32 +0800

    index配置

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,3 @@
-// export const baseUrl = 'https://vip.steedos.com:8443'
-// export const demoSpaceId = 'hSGPc2fsMAp9NDcHy'
+export const baseUrl = 'https://vip.steedos.com:8443'
+export const demoSpaceId = 'hSGPc2fsMAp9NDcHy'
 export const appId = 'wxf7d7403acb34334b'
-
-
-export const baseUrl = 'http://192.168.0.61:5000'
-export const demoSpaceId = 'Af8eM6mAHo7wMDqD3'
