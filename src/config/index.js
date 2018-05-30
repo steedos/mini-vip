6b9e73cf2fcf9497b08fbc90b079e72d6ebc9d9e
Jack Zhuang <zhuangjianguo@me.com>
2018-05-30 15:59:46 +0800

    还原app_id

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const baseUrl = 'https://vip.steedos.com:8443'
 export const demoSpaceId = 'hSGPc2fsMAp9NDcHy'
 export const demoStoreId = 'B2k7E62gWg8DXBztn'
-export const appId = 'wx89eb02efdb4ddaaf'
+export const appId = 'wxf7d7403acb34334b'
