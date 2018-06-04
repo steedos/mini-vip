49f94e1cff69bd24a28fea610619a06762d8b3ce
Jack Zhuang <zhuangjianguo@me.com>
2018-06-04 13:36:33 +0800

    变更服务器地址

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://vip.steedos.com:8443'
+export const baseUrl = 'https://vip-beta.steedos.com/'
 // export const baseUrl = 'http://steedos.ticp.net:3086'
 export const demoSpaceId = 'hSGPc2fsMAp9NDcHy'
 export const demoStoreId = 'B2k7E62gWg8DXBztn'
