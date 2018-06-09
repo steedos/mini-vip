43619c1d649e829e5f9e1ba62e744aaf21fe71bb
Jack Zhuang <zhuangjianguo@me.com>
2018-06-09 17:41:12 +0800

    华炎微站App

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,4 @@
-export const baseUrl = 'https://vip-test.steedos.com:8443'
-// export const baseUrl = 'http://steedos.ticp.net:3086'
+export const baseUrl = 'https://vip.steedos.com'
 export const demoSpaceId = 'hSGPc2fsMAp9NDcHy'
 export const demoStoreId = 'B2k7E62gWg8DXBztn'
-export const appId = 'wx89eb02efdb4ddaaf'
+export const appId = 'wx1c2923e262720536'
