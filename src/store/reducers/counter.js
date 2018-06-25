4dc3777177b5ac48c92edad3520118053c5fd9c3
zhusijia <zhusijia@hotoa.com>
2018-06-25 11:07:04 +0800

    初步完成添加到购物车逻辑

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,5 +1,7 @@
 import { handleActions } from 'redux-actions'
-import { INCREMENT, DECREMENT, ASYNC_INCREMENT } from '../types/counter'
+import { INCREMENT, DECREMENT, ASYNC_INCREMENT, CART_PRODUCT_COUNT, ADD_PRODUCT } from '../types/counter'
+
+const cartList = wx.getStorageSync('cartList') || []
 
 export default handleActions({
   [INCREMENT] (state) {
@@ -19,8 +21,28 @@ export default handleActions({
       ...state,
       asyncNum: state.asyncNum + action.payload
     }
+  },
+  [ADD_PRODUCT] (state, action) {
+    console.log('ADD_PRODUCT......', action);
+    state.cartList.push(action.payload);
+    return {...state}
+  },
+  [CART_PRODUCT_COUNT] (state, action) {
+    const cartList = state.cartList;
+    let cartProductCount = 0;
+    if (cartList && cartList.constructor === Array) {
+      cartList.forEach(p => {
+        cartProductCount += p.count;
+      });
+    }
+    return {
+      ...state,
+      cartProductCount: cartProductCount
+    }
   }
 }, {
   num: 0,
-  asyncNum: 0
+  asyncNum: 0,
+  cartList: cartList,
+  cartProductCount: 0
 })
--- src/store/types/counter.js
+++ src/store/types/counter.js
@@ -3,3 +3,7 @@ export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 
 export const ASYNC_INCREMENT = 'ASYNC_INCREMENT'
+
+export const CART_PRODUCT_COUNT = 'CART_PRODUCT_COUNT'
+
+export const ADD_PRODUCT = 'ADD_PRODUCT'
