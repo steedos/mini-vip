86d4cffef70dc3da0eb0936d708300fdf5adfe53
baozhoutao <baozhoutao@hotoa.com>
2018-07-30 13:55:24 +0800

    #408 新增城市选择

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,10 @@
 export const baseUrl = 'https://vip-test.steedos.com:8443'
 // export const baseUrl = 'http://steedos.ticp.net:3086'
 // export const baseUrl = 'https://vip-beta.steedos.com'
-export const demoSpaceId = 'hSGPc2fsMAp9NDcHy'
-export const demoStoreId = 'B2k7E62gWg8DXBztn'
-export const appId = 'wx033a2ed30bf98cc2'
-export const baseMaterialUrl = 'https://lg-769qcuso-1253849369.cos.ap-shanghai.myqcloud.com/'
-export const loveSpaceId = 'Lnre96ro35Wf9b3gA'
+// export const baseUrl = 'http://192.168.3.2:5000';
+export const demoSpaceId = 'hSGPc2fsMAp9NDcHy';
+export const demoStoreId = 'B2k7E62gWg8DXBztn';
+export const appId = 'wx033a2ed30bf98cc2';
+export const baseMaterialUrl = 'https://lg-769qcuso-1253849369.cos.ap-shanghai.myqcloud.com/';
+export const loveSpaceId = 'Lnre96ro35Wf9b3gA';
+export const txMapsKey = 'GJOBZ-YSX66-K3SSM-E3MJM-IWPRV-BFFIO';
