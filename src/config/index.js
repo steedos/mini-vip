7bfc3068e2c9c058cfd3955abf142bcf7ebcc04d
baozhoutao <baozhoutao@hotoa.com>
2018-09-03 17:04:19 +0800

    #633 fix bug： 切换工作区后，最近访问数据显示错误

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,8 @@
 export const baseUrl = 'http://192.168.3.2:5000';
 export const demoSpaceId = 'hSGPc2fsMAp9NDcHy';
 export const demoStoreId = 'B2k7E62gWg8DXBztn';
-export const appId = 'wxe38d185feee3f4bd';
+export const appId = 'wxf7d7403acb34334b';
 export const baseMaterialUrl = 'https://lg-769qcuso-1253849369.cos.ap-shanghai.myqcloud.com/';
 export const loveSpaceId = 'Lnre96ro35Wf9b3gA';
 export const txMapsKey = 'GJOBZ-YSX66-K3SSM-E3MJM-IWPRV-BFFIO';
-export const phoneLoginShowNickName = false;
+export const phoneLoginShowNickName = true;
