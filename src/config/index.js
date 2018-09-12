5d401e5ce86c8526d6f830e89ee4a640b72e93db
Jack Zhuang <zhuangjianguo@me.com>
2018-09-12 14:57:52 +0800

    删除动态字样

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
-export const baseUrl = 'https://vip-test.steedos.com:8443'
+// export const baseUrl = 'https://vip-test.steedos.com:8443'
 // export const baseUrl = 'http://steedos.ticp.net:3086'
-// export const baseUrl = 'https://vip.steedos.com'
+export const baseUrl = 'https://vip.steedos.com'
 // export const baseUrl = 'http://192.168.3.2:5000';
 export const demoSpaceId = 'hSGPc2fsMAp9NDcHy';
 export const demoStoreId = 'B2k7E62gWg8DXBztn';
