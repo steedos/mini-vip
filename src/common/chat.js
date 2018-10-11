b0dc67e697bce51c04f3631a3dac6e04dd1149fe
baozhoutao <baozhoutao@hotoa.com>
2018-10-11 10:46:25 +0800

    #740 有新消息时，底部tab-bar添加数字提醒

--- src/common/chat.js
+++ src/common/chat.js
@@ -5,151 +5,174 @@ import {chatNewMessagePollingInterval} from '@/config'
 
 class ChatAPI {
 
-  unreadSubscriptions = [];
+	unreadSubscriptions = [];
 
-  _setInterval = null;
+	_setInterval = null;
 
-  //重置未读数量为0
-  async resetUnread(room_id, user_id, space_id) {
-    const queryOptions = {
-      $top: 1,
-      $filter: `(related_to/o eq 'chat_rooms') and (related_to/ids eq '${room_id}') and owner eq '${user_id}' and unread gt 0`,
-      $select: '_id',
-    };
+	//重置未读数量为0
+	async resetUnread(room_id, user_id, space_id) {
+		const queryOptions = {
+			$top: 1,
+			$filter: `(related_to/o eq 'chat_rooms') and (related_to/ids eq '${room_id}') and owner eq '${user_id}' and unread gt 0`,
+			$select: '_id',
+		};
 
-    const result = await ODataClinet.query('chat_subscriptions', queryOptions, space_id);
+		const result = await ODataClinet.query('chat_subscriptions', queryOptions, space_id);
 
-    if (result.value.length > 0) {
-      const sId = result.value[0]._id;
-      ODataClinet.update('chat_subscriptions', sId, {unread: 0}, space_id)
-    }
-  }
+		if (result.value.length > 0) {
+			const sId = result.value[0]._id;
+			ODataClinet.update('chat_subscriptions', sId, {unread: 0}, space_id)
+		}
+	}
 
-  //定时接收通知
-  receivingSubscriptions(user_id, space_id) {
-    if (this._setInterval) {
-      clearInterval(this._setInterval)
-    }
-    this._setInterval = setInterval(async () => {
-      this.getSubscriptions(user_id, space_id).then(res => {
-        const unreadSubscriptions = _.filter(res.value, (s) => {
-          return s.unread > 0 && s.modified_by._id != user_id
-        });
-        this.unreadSubscriptions = unreadSubscriptions;
-      })
-    }, chatNewMessagePollingInterval || 15 * 1000)
-  }
+	//定时接收通知
+	receivingSubscriptions(user_id, space_id) {
+		if (this._setInterval) {
+			clearInterval(this._setInterval)
+		}
+		this._setInterval = setInterval(async () => {
+			this.getSubscriptions(user_id, space_id).then(res => {
+				const unreadSubscriptions = _.filter(res.value, (s) => {
+					return s.unread > 0 && s.modified_by._id != user_id
+				});
+				this.unreadSubscriptions = unreadSubscriptions;
 
-  getSubscriptions(user_id, space_id) {
-    let queryOptions = {
-      $filter: `(related_to/o eq 'chat_rooms') and owner eq '${user_id}'`,
-      $select: 'last_message_text,unread,modified_by,related_to',
-      $expand: 'modified_by($select=name,avatarUrl)'
-    };
-    return ODataClinet.query('chat_subscriptions', queryOptions, space_id)
-  }
+				//tab上显示小红点
+				// if (unreadSubscriptions.length > 0) {
+				// 	wx.showTabBarRedDot({index: 1})
+				// } else {
+				// 	wx.hideTabBarRedDot({index: 1})
+				// }
 
-  async getRoom(user_id, space, room_members) {
-    let filter = [];
+				//tab上显示文字
+				let unreadCount = 0;
+				_.forEach(unreadSubscriptions, (unreadSubscription)=>{
+					unreadCount = unreadCount + (unreadSubscription.unread || 0)
+				});
+				console.log('unreadCount', unreadCount)
+				if (unreadCount > 0) {
+					if(unreadCount > 999){
+						unreadCount = 999
+					}
+					wx.setTabBarBadge({index: 1, text: unreadCount.toString()})
+				} else {
+					wx.removeTabBarBadge({index: 1})
+				}
 
-    _.forEach(room_members, (m) => {
-      filter.push(`(members eq '${m}')`)
-    });
+			})
+		}, chatNewMessagePollingInterval || 15 * 1000)
+	}
 
-    let queryOptions = {
-      $filter: `(type eq 'private') and (${filter.join(' and ')})`,
-      $select: '_id,name',
-    };
+	getSubscriptions(user_id, space_id) {
+		let queryOptions = {
+			$filter: `(related_to/o eq 'chat_rooms') and owner eq '${user_id}'`,
+			$select: 'last_message_text,unread,modified_by,related_to',
+			$expand: 'modified_by($select=name,avatarUrl)'
+		};
+		return ODataClinet.query('chat_subscriptions', queryOptions, space_id)
+	}
 
-    const room = await ODataClinet.query('chat_rooms', queryOptions, space);
+	async getRoom(user_id, space, room_members) {
+		let filter = [];
 
-    console.log('getRoom room', room);
-    if (room.value.length > 0) {
-      return room.value[0]
-    } else {
+		_.forEach(room_members, (m) => {
+			filter.push(`(members eq '${m}')`)
+		});
 
-      const room_data = {
-        type: 'private',
-        name: '',
-        owner: user_id,
-        members: room_members
-      };
-      const insert_room = await ODataClinet.insert('chat_rooms', room_data, space);
-      console.log('getRoom insert_room', insert_room);
-      if (insert_room.value.length > 0) {
-        return insert_room.value[0]
-      }
-      ;
-    }
-  }
+		let queryOptions = {
+			$filter: `(type eq 'private') and (${filter.join(' and ')})`,
+			$select: '_id,name',
+		};
 
-  sendMsg(room_id, msg, type = 'text', space, user_id, messageShowTime) {
-    return this.msg(room_id, msg, type, space, user_id, messageShowTime);
-  }
+		const room = await ODataClinet.query('chat_rooms', queryOptions, space);
 
-  getHistory(room_id, space, top, filter) {
+		console.log('getRoom room', room);
+		if (room.value.length > 0) {
+			return room.value[0]
+		} else {
 
-    let queryOptions = {
-      $top: top || 15,
-      $filter: `(related_to/o eq 'chat_rooms') and (related_to/ids eq '${room_id}')`,
-      $orderby: 'created desc',
-      $select: 'name,type,created,owner,display_time',
-      $expand: 'owner($select=name,avatarUrl)'
-    };
+			const room_data = {
+				type: 'private',
+				name: '',
+				owner: user_id,
+				members: room_members
+			};
+			const insert_room = await ODataClinet.insert('chat_rooms', room_data, space);
+			console.log('getRoom insert_room', insert_room);
+			if (insert_room.value.length > 0) {
+				return insert_room.value[0]
+			}
+			;
+		}
+	}
 
-    if (filter) {
-      queryOptions.$filter = `${queryOptions.$filter} and ${filter}`
-    }
+	sendMsg(room_id, msg, type = 'text', space, user_id, messageShowTime) {
+		return this.msg(room_id, msg, type, space, user_id, messageShowTime);
+	}
 
-    return ODataClinet.query('chat_messages', queryOptions, space)
-  }
+	getHistory(room_id, space, top, filter) {
 
-  getNewMessage(room_id, space, timestamp) {
-    let queryOptions = {
-      $filter: `(related_to/o eq 'chat_rooms') and (related_to/ids eq '${room_id}') and created gt ${timestamp}`,
-      $orderby: 'created desc',
-      $select: 'name,type,created,owner,display_time',
-      $expand: 'owner($select=name,avatarUrl)'
-    };
-    return ODataClinet.query('chat_messages', queryOptions, space)
-  }
+		let queryOptions = {
+			$top: top || 15,
+			$filter: `(related_to/o eq 'chat_rooms') and (related_to/ids eq '${room_id}')`,
+			$orderby: 'created desc',
+			$select: 'name,type,created,owner,display_time',
+			$expand: 'owner($select=name,avatarUrl)'
+		};
 
-  async msg(room_id, msg, type = 'text', space, user_id, display_time) {
-    let data = {
-      related_to: {
-        o: "chat_rooms",
-        ids: [room_id]
-      },
-      name: msg,
-      type: 'text',
-      owner: user_id,
-      display_time: display_time
-    };
+		if (filter) {
+			queryOptions.$filter = `${queryOptions.$filter} and ${filter}`
+		}
 
-    const res = await ODataClinet.insert('chat_messages', data, space);
+		return ODataClinet.query('chat_messages', queryOptions, space)
+	}
 
-    // data._id = res.value[0]._id;
-    data = res.value[0];
+	getNewMessage(room_id, space, timestamp) {
+		let queryOptions = {
+			$filter: `(related_to/o eq 'chat_rooms') and (related_to/ids eq '${room_id}') and created gt ${timestamp}`,
+			$orderby: 'created desc',
+			$select: 'name,type,created,owner,display_time',
+			$expand: 'owner($select=name,avatarUrl)'
+		};
+		return ODataClinet.query('chat_messages', queryOptions, space)
+	}
 
-    return data;
+	async msg(room_id, msg, type = 'text', space, user_id, display_time) {
+		let data = {
+			related_to: {
+				o: "chat_rooms",
+				ids: [room_id]
+			},
+			name: msg,
+			type: 'text',
+			owner: user_id,
+			display_time: display_time
+		};
 
-    // let history = wepy.getStorageSync('_wechat_history_') || m_history;
-    // let msgObj = {
-    //   to: to,
-    //   msg: msg,
-    //   type: type,
-    //   from: frm,
-    //   time: +new Date()
-    // };
-    //
-    // history.push(msgObj);
-    //
-    // return new Promise((resolve, reject) => {
-    //   wepy.setStorage({key: '_wechat_history_', data: history}).then(() => {
-    //     resolve(msgObj);
-    //   }).catch(reject);
-    // });
-  }
+		const res = await ODataClinet.insert('chat_messages', data, space);
+
+		// data._id = res.value[0]._id;
+		data = res.value[0];
+
+		return data;
+
+		// let history = wepy.getStorageSync('_wechat_history_') || m_history;
+		// let msgObj = {
+		//   to: to,
+		//   msg: msg,
+		//   type: type,
+		//   from: frm,
+		//   time: +new Date()
+		// };
+		//
+		// history.push(msgObj);
+		//
+		// return new Promise((resolve, reject) => {
+		//   wepy.setStorage({key: '_wechat_history_', data: history}).then(() => {
+		//     resolve(msgObj);
+		//   }).catch(reject);
+		// });
+	}
 }
 
 export default new ChatAPI()
