c5517b7ae9c8dbf53acfaa25afdd27852df27247
baozhoutao <baozhoutao@hotoa.com>
2018-10-13 18:00:28 +0800

    #704 添加统一的odata method调用函数

--- src/utils/odata_client.js
+++ src/utils/odata_client.js
@@ -48,6 +48,7 @@ class ODataClinet{
     return result
   }
 
+  //调用 odata method, 具体使用方式见文档: https://github.com/steedos/help/blob/master/zh-cn/creator/odata_functions.md
   async call_method(method_name, body_params, object_name, _id, space_id){
     const url = `/api/odata/v4/${space_id}/${object_name}/${_id}/${method_name}`;
     const result = await req.post(url, body_params || {}).catch((err)=>{
