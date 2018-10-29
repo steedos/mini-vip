8cf5339921ad8724d8be1ec661ad8f1c831fd26b
baozhoutao <baozhoutao@hotoa.com>
2018-10-29 18:07:07 +0800

    动态功能引入socket.io

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
-export const baseUrl = 'https://vip-test.steedos.com:8443'
+// export const baseUrl = 'https://vip-test.steedos.com:8443'
 // export const baseUrl = 'http://steedos.ticp.net:3086'
 // export const baseUrl = 'https://vip.steedos.com'
-// export const baseUrl = 'http://192.168.3.2:5000';
+export const baseUrl = 'http://192.168.3.2:5000';
 export const demoSpaceId = 'hSGPc2fsMAp9NDcHy';
 export const demoStoreId = 'B2k7E62gWg8DXBztn';
 export const appId = 'wxf7d7403acb34334b';
@@ -10,7 +10,7 @@ export const loveSpaceId = 'Lnre96ro35Wf9b3gA';
 export const txMapsKey = 'GJOBZ-YSX66-K3SSM-E3MJM-IWPRV-BFFIO';
 export const phoneLoginShowNickName = true;
 
-export const socketServer = 'http://vip-test.steedos.com:8445';
-export const chatBadgeTabBarIndex = 1;
+export const socketServer = 'http://192.168.3.2:8080';
+// export const chatBadgeTabBarIndex = 1;
 
 export const chatNewMessagePollingInterval = 15 * 1000;
